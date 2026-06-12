fb96a75b6bb6ec56ed1397991980da326a9870d5
Mayank Dave <mayankdave255@gmail.com>
2026-06-12 23:30:51 +0530

    basic input output

---
+++ day1/basic_revision.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World"<<endl;
+    cout<<"Adding Two numbers";
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b;
+    
+    
+    
+    return 0;
+}
