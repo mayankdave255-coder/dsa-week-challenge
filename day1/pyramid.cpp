456ea4665995ec52216c6c6995cbb86e940b6c8e
Mayank Dave <mayankdave255@gmail.com>
2026-06-13 00:36:40 +0530

    pyramid patterns

---
+++ day1/pyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int side;
+    cin>>side;
+    for(int i=0;i<side;i++){
+        for(int j=0;j<side-i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<((2*i)+1);j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
