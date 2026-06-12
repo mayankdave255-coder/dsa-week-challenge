12c95f2381bd3c48387b862e486cc45438a18030
Mayank Dave <mayankdave255@gmail.com>
2026-06-13 00:03:17 +0530

    Hollow rectangle Pattern

---
+++ day1/hollow_rectangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int l,b;
+    cin>>l>>b;
+    for(int i=0;i<l;i++){
+        for(int j=0;j<b;j++){
+            if(i==0||i==l-1||j==0||j==b-1){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
