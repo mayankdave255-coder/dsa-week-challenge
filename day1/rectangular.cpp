ee6f553858190767909a29311ffaeeac94623418
Mayank Dave <mayankdave255@gmail.com>
2026-06-12 23:54:49 +0530

    rectangular pattern

---
+++ day1/rectangular.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int l,b;
+    cin>>l>>b;
+    for(int i=0;i<l;i++){
+        for(int j=0;j<b;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
